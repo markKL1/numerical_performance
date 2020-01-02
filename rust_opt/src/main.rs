fd4439869598593b4325196bd54890b2c116489f
Mark Verleg <Mark.Verleg@keylane.com>
2020-01-02 14:59:51 +0100

    Remove bound checks when indexing

--- rust_opt/src/main.rs
+++ rust_opt/src/main.rs
@@ -39,17 +39,21 @@ fn mat_mul(n: usize, A: Vec<f64>, B: Vec<f64>) -> Vec<f64> {
             let mut k = n;
             // Deal with the remainder modulo 4.
             for k in (n - n % STEP) .. n {
-                C[ni + j] += A[ni + k] * Bc[k];
+                unsafe {
+                    C[ni + j] += A.get_unchecked(ni + k) * Bc.get_unchecked(k);
+                }
             }
             // Do the rest in steps of 4.
             let mut sums = [0.0; STEP];
             loop {
                 k -= STEP;
                 debug_assert!(STEP == 4);
-                sums[0] += A[ni + k + 0] * Bc[k+ 0];
-                sums[1] += A[ni + k + 1] * Bc[k + 1];
-                sums[2] += A[ni + k + 2] * Bc[k + 2];
-                sums[3] += A[ni + k + 3] * Bc[k + 3];
+                unsafe {
+                    sums[0] += A.get_unchecked(ni + k + 0) * Bc.get_unchecked(k + 0);
+                    sums[1] += A.get_unchecked(ni + k + 1) * Bc.get_unchecked(k + 1);
+                    sums[2] += A.get_unchecked(ni + k + 2) * Bc.get_unchecked(k + 2);
+                    sums[3] += A.get_unchecked(ni + k + 3) * Bc.get_unchecked(k + 3);
+                };
                 if k <= 0 {
                     break;
                 }
