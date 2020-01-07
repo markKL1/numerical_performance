fd27af4528da794a443ca5d1219189b1b3675ec5
Mark Verleg <Mark.Verleg@keylane.com>
2020-01-07 11:25:41 +0100

    Chunked mutable iteration

--- rust_3par/src/lib.rs
+++ rust_3par/src/lib.rs
@@ -5,6 +5,10 @@ use ::std::time::Instant;
 use ::par_array_init::par_array_init;
 use ::rayon::iter::IntoParallelIterator;
 use ::rayon::iter::ParallelIterator;
+use ::rayon::iter::IntoParallelRefIterator;
+use ::rayon::slice::ParallelSliceMut;
+
+const STEP: usize = 8;
 
 fn make_mat_empty(n: usize) -> Vec<f64> {
     let mut v: Vec<f64> = Vec::with_capacity(n * n);
@@ -39,15 +43,56 @@ pub fn mat_mul(n: usize, A: Vec<f64>, B: Vec<f64>) -> Vec<f64> {
         Bc
     }).collect::<Vec<_>>();
 
+    //TODO @mverleg: https://docs.rs/rayon/1.3.0/rayon/slice/trait.ParallelSliceMut.html#tymethod.as_parallel_slice_mut
+
     let mut C = make_mat_empty(n);
-    C.as_mut_slice().into_par_iter().batched();
+    //TODO @mverleg: should this be called row or column?
+    C.par_chunks_mut(n)
+        .for_each(|row| {
+            let j = 0;  //TODO @mverleg: TEMPORARY! REMOVE THIS!
+            dbg!(row.len());
+            let Bc = &BT[j];
+
+            for i in 0 .. n {
+                let ni = i * n;
+                let mut k = 0;
+                let stop_simd_at = n - (n % STEP);
+                while k < stop_simd_at {
+                    debug_assert!(8 == STEP);
+                    let nik = ni + k;
+                    unsafe {
+                        row[j] += A.get_unchecked(nik + 0) * Bc.get_unchecked(k + 0);
+                        row[j] += A.get_unchecked(nik + 1) * Bc.get_unchecked(k + 1);
+                        row[j] += A.get_unchecked(nik + 2) * Bc.get_unchecked(k + 2);
+                        row[j] += A.get_unchecked(nik + 3) * Bc.get_unchecked(k + 3);
+                        row[j] += A.get_unchecked(nik + 4) * Bc.get_unchecked(k + 4);
+                        row[j] += A.get_unchecked(nik + 5) * Bc.get_unchecked(k + 5);
+                        row[j] += A.get_unchecked(nik + 6) * Bc.get_unchecked(k + 6);
+                        row[j] += A.get_unchecked(nik + 7) * Bc.get_unchecked(k + 7);
+                    }
+                    k += STEP;
+                }
+                for k in stop_simd_at .. n {
+                    unsafe {
+                        row[j] += A.get_unchecked(ni + k) * Bc.get_unchecked(k);
+                    }
+                }
+            }
+
+        });
+
+    //TODO @mverleg: sequential_threshold(n)
+//    (0 .. n).into_par_iter()
+//        .map(|i| C[i .. i + n])
+//        .for_each(|s| s.len());
+
+//
+//    C.as_mut_slice().into_par_iter().batched();
 //    C.as_mut_slice().into_par_iter()
 //        .batch
 //        .zip((0 .. Array::len()).into_par_iter().map(|i| initializer(i)))
 //        //TODO @mverleg: batched?
 //        .for_each(|dst, src| *dst = src);
-
-
 
 //    let iter = into_iter.into_par_iter();
 //    if Array::len() > iter.len() {
