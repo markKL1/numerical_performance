7aa69afdb56a8660ed8269d36c7b87449d1498d4
Mark Verleg <Mark.Verleg@keylane.com>
2020-01-07 14:51:06 +0100

    Started Java project, but without mvn run, because that did not work

---
+++ java_jni/src/com/keylane/JniTest.java
@@ -0,0 +1,19 @@
+
+class JniTest {
+    // This declares that the static `hello` method will be provided
+    // a native library.
+    private static native String hello(String input);
+
+    static {
+        // This actually loads the shared object that we'll be creating.
+        // The actual location of the .so or .dll may differ based on your
+        // platform.
+        System.loadLibrary("mylib");
+    }
+
+    // The rest is just regular ol' Java!
+    public static void main(String[] args) {
+        String output = JniTest.hello("josh");
+        System.out.println(output);
+    }
+}
