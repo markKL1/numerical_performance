e41c1ece0efc658712f5a88fe182c67557029437
Mark Verleg <Mark.Verleg@keylane.com>
2020-01-07 11:25:09 +0100

    Fix a bug in the loop unrolling code

--- rust_1opt/src/main.rs
+++ rust_1opt/src/main.rs
@@ -37,26 +37,31 @@ fn mat_mul(n: usize, A: Vec<f64>, B: Vec<f64>) -> Vec<f64> {
             }
         }
         for i in 0..n {
+            let mut sums = [0.0; STEP];
             let ni = i * n;
+            let nij = ni + j;
             let mut k = 0;
             let stop_simd_at = n - (n % STEP);
             while k < stop_simd_at {
                 debug_assert!(8 == STEP);
+                let nik = ni + k;
+                //TODO @mverleg: how can this simd if they all have the same target?
                 unsafe {
-                    C[ni + j] += A.get_unchecked(ni + k + 0) * Bc.get_unchecked(k + 0);
-                    C[ni + j] += A.get_unchecked(ni + k + 1) * Bc.get_unchecked(k + 1);
-                    C[ni + j] += A.get_unchecked(ni + k + 2) * Bc.get_unchecked(k + 2);
-                    C[ni + j] += A.get_unchecked(ni + k + 3) * Bc.get_unchecked(k + 3);
-                    C[ni + j] += A.get_unchecked(ni + k + 4) * Bc.get_unchecked(k + 4);
-                    C[ni + j] += A.get_unchecked(ni + k + 5) * Bc.get_unchecked(k + 5);
-                    C[ni + j] += A.get_unchecked(ni + k + 6) * Bc.get_unchecked(k + 6);
-                    C[ni + j] += A.get_unchecked(ni + k + 7) * Bc.get_unchecked(k + 7);
+                    sums[0] += A.get_unchecked(nik + 0) * Bc.get_unchecked(k + 0);
+                    sums[1] += A.get_unchecked(nik + 1) * Bc.get_unchecked(k + 1);
+                    sums[2] += A.get_unchecked(nik + 2) * Bc.get_unchecked(k + 2);
+                    sums[3] += A.get_unchecked(nik + 3) * Bc.get_unchecked(k + 3);
+                    sums[4] += A.get_unchecked(nik + 4) * Bc.get_unchecked(k + 4);
+                    sums[5] += A.get_unchecked(nik + 5) * Bc.get_unchecked(k + 5);
+                    sums[6] += A.get_unchecked(nik + 6) * Bc.get_unchecked(k + 6);
+                    sums[7] += A.get_unchecked(nik + 7) * Bc.get_unchecked(k + 7);
                 }
                 k += STEP;
             }
+            C[nij] = sums.iter().sum();
             for k in stop_simd_at .. n {
                 unsafe {
-                    C[ni + j] += A.get_unchecked(ni + k) * Bc.get_unchecked(k);
+                    C[nij] += A.get_unchecked(ni + k) * Bc.get_unchecked(k);
                 }
             }
         }
